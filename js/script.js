ed1c235b057ddead70916a69fc6feb52775a9bd3
yazn_108 <102805404+yazn-108@users.noreply.github.com>
2023-08-25 08:36:44 +0300

    CRUDS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 "use strict";
 let
+allPriceInputs = document.querySelectorAll(".inputs .price input"),
 title = document.querySelector(".inputs #title"),
 price = document.querySelector(".inputs #price"),
 taxes = document.querySelector(".inputs #taxes"),
@@ -12,98 +13,82 @@ submit = document.querySelector(".inputs #submit");
 let systemMode = "create";
 let tmp;
 let searchMode = "search-by-title";
-function getTotal(){
-    if(price.value != ""){
-        let result = (+price.value + +taxes.value + +ads.value) - +discount.value;
-        total.innerHTML = result;
-    }else{
-        total.innerHTML = '';
-    };
-};
-let productData = JSON.parse(localStorage.getItem("productsData")) || [];
-submit.addEventListener("click",() => {
-    if(price.value != ""){
-        let productObject = {
-            title: title.value.toLowerCase(),
-            price: price.value,
-            taxes: taxes.value,
-            ads: ads.value,
-            discount: discount.value,
-            total: total.innerHTML,
-            count: count.value,
-            category: category.value.toLowerCase()
-        }; 
-        if(systemMode === "create"){
-            if(productObject.count > 1){
-                for(let i = 0; i < productObject.count; i++){
-                    productData.push(productObject);
-                };
-            }else{productData.push(productObject);};
-        }else{
-            productData[tmp] = productObject;
-            systemMode = "create";
-            submit.innerHTML = systemMode;
-            count.style.pointerEvents = "";
-        };
-        localStorage.setItem("productsData",JSON.stringify(productData));
-        clearInputs();
-        showData();
-    }else{
-        price.focus();
-    };
+allPriceInputs.forEach(input => {
+    input.addEventListener("keyup",() => {
+        total.innerHTML = (+price.value + +taxes.value + +ads.value) - +discount.value;
+    });
 });
-function clearInputs(){
+let proData = JSON.parse(localStorage.getItem("localData")) || [];
+submit.addEventListener("click", () => {
+    let object = {
+        title: title.value,
+        price: price.value,
+        taxes: taxes.value,
+        ads: ads.value,
+        discount: discount.value,
+        total: total.innerHTML,
+        count: count.value,
+        category: category.value
+    };
+    if (systemMode == "create") {
+        if(count.value > 1){
+            for(let i = 0; i < count.value; i++) {proData.push(object);};
+        }else{proData.push(object);};
+        
+    } else {
+        proData[tmp] = object;
+        systemMode = "create";
+        count.style.pointerEvents = "";
+    };
+    localStorage.setItem("localData",JSON.stringify(proData));
+    show();
     document.querySelectorAll(".inputs input").forEach(e => e.value = "");
-    total.innerHTML = '';
-};
-function showData(){
-    let product = '';
-    for(let i = 0; i < productData.length; i++){
-        product += `
+    total.innerHTML = "";
+});
+function show(){
+    let items = "";
+    for(let i=0; i < proData.length; i++){
+        items +=`
         <tr>
             <td>${i+1}</td>
-            <td>${productData[i].title}</td>
-            <td>${productData[i].price}</td>
-            <td>${productData[i].taxes}</td>
-            <td>${productData[i].ads}</td>
-            <td>${productData[i].discount}</td>
-            <td>${productData[i].total}</td>
-            <td>${productData[i].category}</td>
+            <td>${proData[i].title}</td>
+            <td>${proData[i].price}</td>
+            <td>${proData[i].taxes}</td>
+            <td>${proData[i].ads}</td>
+            <td>${proData[i].discount}</td>
+            <td>${proData[i].total}</td>
+            <td>${proData[i].category}</td>
             <td><button id="update" onclick="updateProduct(${i})">update</button></td>
             <td><button id="delete" onclick="deleteProduct(${i})">delete</button></td>
-        </tr>
-    `};
-    document.querySelector("table tbody").innerHTML = product;
-    let deleteAll = document.querySelector("#deleteAll");
-    productData.length > 0
-        ?deleteAll.innerHTML = `<button onclick="deleteAll()">Delete all (${productData.length})</button>`
-        :deleteAll.innerHTML = "";
-};
-showData();
-function deleteProduct(i){
-    if(systemMode == "create"){
-        productData.splice(i,1);
-        localStorage.productsData = JSON.stringify(productData);
-        showData();
+        </tr>`
     };
+    document.querySelector("tbody").innerHTML = items;
+    proData.length > 0
+    ?document.querySelector("#deleteAll").innerHTML = 
+    `<button onclick="deleteAll()">delete All (${proData.length})</button>`
+    :document.querySelector("#deleteAll").innerHTML = "";
+};
+show();
+function deleteProduct(i){
+    proData.splice(i,1)
+    localStorage.setItem("localData",JSON.stringify(proData));
+    show();
 };
 function deleteAll(){
-    if(systemMode == "create"){
-        localStorage.clear();
-        productData.splice(0);
-        showData();
-    };
+    localStorage.clear();
+    proData.splice(0);
+    show();
 };
 function updateProduct(i){
-    tmp = i;
-    title.value = productData[i].title
-    price.value = productData[i].price;
-    taxes.value = productData[i].taxes;
-    ads.value = productData[i].ads;
-    discount.value = productData[i].discount;
-    total.innerHTML = productData[i].total;
+    tmp = i
+    title.value = proData[i].title;
+    price.value = proData[i].price;
+    taxes.value = proData[i].taxes;
+    ads.value = proData[i].ads;
+    discount.value = proData[i].discount;
+    total.innerHTML = proData[i].total;
+    category.value = proData[i].category;
     count.style.pointerEvents = "none";
-    category.value = productData[i].category;
     systemMode = "update";
     submit.innerHTML = systemMode;
     scrollTo({
@@ -111,49 +96,55 @@ function updateProduct(i){
         behavior: "smooth"
     });
 };
-function getSearchMode(id){
-    id == 'search-by-title'?searchMode = id:searchMode = id;
-    document.querySelector('#search').placeholder = searchMode.replace(/\W/g, ' ');
-    document.querySelector('#search').focus();
-    document.querySelector('#search').value = "";
-    showData();
-};
-function searchData(value){
-    let product = "";
-    for(var i = 0; i < productData.length; i++){
+let searchButtons = document.querySelectorAll('.search-buttons button');
+let searchInput = document.querySelector('.outputs #search');
+searchButtons.forEach(button => {
+    button.addEventListener('click',buttonId => {
+        let id = buttonId.target.id;
+        id == 'search-by-title'?searchMode = id:searchMode = id;
+        searchInput.placeholder = searchMode.replace(/\W/g, ' ');
+        searchInput.focus();
+        searchInput.value = "";
+        show();
+    });
+});
+let searchValue = document.querySelector(".outputs #search");
+searchValue.addEventListener("keyup", () => {
+    let items = "";
+    for(var i = 0; i < proData.length; i++){
         if(searchMode == "search-by-title"){
-            if(productData[i].title.includes(value.toLowerCase())){
-                product += `
+            if(proData[i].title.includes(searchValue.value.toLowerCase())){
+                items += `
                 <tr>
                     <td>${i}</td>
-                    <td>${productData[i].title}</td>
-                    <td>${productData[i].price}</td>
-                    <td>${productData[i].taxes}</td>
-                    <td>${productData[i].ads}</td>
-                    <td>${productData[i].discount}</td>
-                    <td>${productData[i].total}</td>
-                    <td>${productData[i].category}</td>
+                    <td>${proData[i].title}</td>
+                    <td>${proData[i].price}</td>
+                    <td>${proData[i].taxes}</td>
+                    <td>${proData[i].ads}</td>
+                    <td>${proData[i].discount}</td>
+                    <td>${proData[i].total}</td>
+                    <td>${proData[i].category}</td>
                     <td><button id="update" onclick="updateProduct(${i})">update</button></td>
                     <td><button id="delete" onclick="deleteProduct(${i})">delete</button></td>
-                </tr>` 
+                </tr>`
             };
         }else{
-            if(productData[i].category.includes(value.toLowerCase())){
-                product += `
+            if(proData[i].category.includes(searchValue.value.toLowerCase())){
+                items += `
                 <tr>
                     <td>${i}</td>
-                    <td>${productData[i].title}</td>
-                    <td>${productData[i].price}</td>
-                    <td>${productData[i].taxes}</td>
-                    <td>${productData[i].ads}</td>
-                    <td>${productData[i].discount}</td>
-                    <td>${productData[i].total}</td>
-                    <td>${productData[i].category}</td>
+                    <td>${proData[i].title}</td>
+                    <td>${proData[i].price}</td>
+                    <td>${proData[i].taxes}</td>
+                    <td>${proData[i].ads}</td>
+                    <td>${proData[i].discount}</td>
+                    <td>${proData[i].total}</td>
+                    <td>${proData[i].category}</td>
                     <td><button id="update" onclick="updateProduct(${i})">update</button></td>
                     <td><button id="delete" onclick="deleteProduct(${i})">delete</button></td>
                 </tr>` 
             };
         };
     };
-    document.querySelector("table tbody").innerHTML = product;
-};
+    document.querySelector("table tbody").innerHTML = items;  
+});
